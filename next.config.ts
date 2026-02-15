277fa5ed83104aabf0477914355263b4ae3c7a5e
Serge <serge@schef.app>
2026-02-15 16:23:02 +0600

    chore: disable Sentry for debug

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSentryConfig(nextConfig, {
-  // Тихая сборка без лишних логов
-  silent: true,
-
-  // Не загружать source maps если нет auth token
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-
-  // Расширить загрузку клиентских файлов
-  widenClientFileUpload: true,
-
-  // Отключить логгер Sentry
-  disableLogger: true,
-});
+export default nextConfig;
